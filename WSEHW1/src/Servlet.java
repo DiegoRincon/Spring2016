6e92d42b76998e1d517de908edea1a233084391e
Diego Rincon <drs414@nyu.edu>
2016-02-15 19:09:56 -0500

    servlet

--- WSEHW1/src/Servlet.java
+++ WSEHW1/src/Servlet.java
@@ -7,35 +7,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Servlet extends HttpServlet {
-	private static final long serialVersionUID = 8778174121968337225L;
+   private static final long serialVersionUID = 8778174121968337225L;
 
-	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		// Set the response message's MIME type
-		response.setContentType("text/html;charset=UTF-8");
-		// Allocate a output writer to write the response message into the network socket
-		PrintWriter out = response.getWriter();
+   @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+   throws IOException, ServletException {
+   // Set the response message's MIME type
+   response.setContentType("text/html;charset=UTF-8");
+   // Allocate a output writer to write the response message into the network socket
+   PrintWriter out = response.getWriter();
 
-		// Write the response message, in an HTML page
-		try {
-			out.println("<!DOCTYPE html>");
-			out.println("<html><head>");
-			out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
-			out.println("<title>Hello, World</title></head>");
-			out.println("<body>");
-			out.println("<h1>Hello, world!</h1>");  // says Hello
-			// Echo client's request information
-			out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
-			out.println("<p>Protocol: " + request.getProtocol() + "</p>");
-			out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
-			out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
-			// Generate a random number upon each request
-			out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
-			out.println("</body>");
-			out.println("</html>");
-		} finally {
-			out.close();  // Always close the output writer
-		}
-	}
+   // Write the response message, in an HTML page
+   try {
+      out.println("<!DOCTYPE html>");
+      out.println("<html><head>");
+      out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+      out.println("<title>Search Results</title></head>");
+      out.println("<body>");
+      // Echo client's request information
+      String query = request.getParameter("query");
+      out.println("<p>Your query was: " + query + "</p>");
+      out.println("<p>THIS IS A CHANGE</p>");
+      out.println("</body>");
+      out.println("</html>");
+   } finally {
+      out.close();  // Always close the output writer
+   }
+   }
 }
